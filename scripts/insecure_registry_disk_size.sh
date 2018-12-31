cb73fd48961073aa500ddba26a4c6b80b79a7eb8
Tomas <tcarneado@gmail.com>
2018-12-31 16:03:13 +0100

    insecure registry and disk size script

---
+++ scripts/insecure_registry_disk_size.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#Install Docker Toolbox without Virtualbox and with git
+
+machine=default
+disk_size=50000
+insecure_registry_ip=192.168.99.100
+insecure_registry_port=55000
+
+docker-machine stop $machine
+docker-machine rm $machine
+docker-machine create -d virtualbox --virtualbox-disk-size ""$disk_size"" --engine-insecure-registry=$insecure_registry_ip:$insecure_registry_port $machine
