c55faf7c3d3f82737c94389fd4e9f3f18df23509
Tomas <tcarneado@gmail.com>
2019-01-04 17:53:44 +0100

    downgrade docker to 18.06

--- scripts/insecure_registry_disk_size.sh
+++ scripts/insecure_registry_disk_size.sh
@@ -8,6 +8,9 @@
 # https://docs.docker.com/machine/reference/create/#accessing-driver-specific-flags-in-the-help-text
 # --virtualbox-cpu-count "2" 
 # --virtualbox-memory "1024"
+# https://github.com/docker/machine/issues/4608
+# --virtualbox-boot2docker-url "https://github.com/boot2docker/boot2docker/releases/download/v18.06.1-ce/boot2docker.iso"
+# docker-machine create -d virtualbox --virtualbox-disk-size "60000" --engine-insecure-registry=192.168.99.100:55000 --virtualbox-cpu-count "2"  --virtualbox-memory "1024" --virtualbox-boot2docker-url "https://github.com/boot2docker/boot2docker/releases/download/v18.06.1-ce/boot2docker.iso" default
 
 machine=default
 disk_size=50000
